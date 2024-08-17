d1f98213a675602b8d2dd9bf70e66679d804e920
Greg Sherman <63402349+GregSherman@users.noreply.github.com>
2024-08-16 18:02:06 -0700

    add mongodb atlas support

--- src/services/db.js
+++ src/services/db.js
@@ -1,25 +1,28 @@
 import { MongoClient } from 'mongodb';
 
-const url = 'mongodb://localhost:27017'; // Local MongoDB URL
-const dbName = 'musicDB';
-
-async function connect() {
-  const client = new MongoClient(url);
-  await client.connect();
-  return client.db(dbName);
-}
-
 class DatabaseService {
+  constructor() {
+    this.MongoURI = process.env.MONGO_URI;
+    this.dbName = 'musicDB';
+  }
+
+  async connect() {
+    const client = new MongoClient(this.MongoURI);
+    await client.connect();
+    console.log('Connected to database');
+    return client.db(this.dbName);
+  }
+
   async getSongMetadata(trackId) {
     console.log('Retrieving metadata for trackId', trackId);
-    const db = await connect();
+    const db = await this.connect();
     const track = await db.collection('tracks').findOne({ trackId });
     return track;
   }
 
   async saveSongMetadata(metadata) {
     console.log('Saving metadata for trackId', metadata.trackId);
-    const db = await connect();
+    const db = await this.connect();
     const song = {
       trackId: metadata.trackId,
       title: metadata.title,
@@ -41,7 +44,7 @@ class DatabaseService {
 
   async markSongAsPlayed(trackId) {
     console.log('Marking song as played:', trackId);
-    const db = await connect();
+    const db = await this.connect();
     await db.collection('tracks').updateOne(
       { trackId },
       {
@@ -52,7 +55,7 @@ class DatabaseService {
   }
 
   async getRecentlyPlayedSongs(hours) {
-    const db = await connect();
+    const db = await this.connect();
     const cutoff = new Date(Date.now() - hours * 3600 * 1000);
     const songs = await db
       .collection('tracks')
@@ -68,7 +71,7 @@ class DatabaseService {
   }
 
   async getLastPlayedSongs(limit) {
-    const db = await connect();
+    const db = await this.connect();
     const songs = await db.collection('tracks').find().sort({ lastPlayed: -1 }).limit(limit).toArray();
     console.log(
       'Last played',
