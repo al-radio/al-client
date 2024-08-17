8a606a3e85a348f13c7f615b4d6ffe504ece5c04
Greg Sherman <63402349+GregSherman@users.noreply.github.com>
2024-08-16 18:32:09 -0700

    have a setup aduio script

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import songRoutes from './routes/routes.js';
 import SongController from './controllers/songController.js';
 import SpotifyService from './services/spotify.js';
-import fs from 'fs';
 import path from 'path';
 
 const app = express();
@@ -13,9 +12,6 @@ app.use('/', songRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   console.log(`Server is running at http://localhost:${PORT}`);
-  fs.readdirSync('./audio').forEach(file => {
-    fs.unlinkSync(`./audio/${file}`);
-  });
   await SpotifyService.authenticate();
   await SongController.getNextSong();
   SongController.player();
