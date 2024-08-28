627c2bcb991317a33de44ac4b34a9573b7fc768c
Greg Sherman <63402349+GregSherman@users.noreply.github.com>
2024-08-28 16:01:16 -0700

    persist window visibility

--- contexts/VisibilityContext.js
+++ contexts/VisibilityContext.js
@@ -1,23 +1,53 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const VisibilityContext = createContext();
 
+const getLocalStorageKey = (component) => `visibility_${component}`;
+
 export const VisibilityProvider = ({ children }) => {
-  const [visibility, setVisibility] = useState({
+  const initialVisibility = {
     audioPlayer: true,
     songHistory: true,
     nextSong: true,
     submitSong: true,
     listeners: true,
-    account: true,
-    customize: true,
-  });
+    account: false,
+    customize: false,
+  };
 
+  // Function to load visibility state from localStorage
+  const loadVisibilityState = () => {
+    if (typeof window === "undefined") return initialVisibility; // Return initial state on the server
+
+    const savedVisibility = {};
+    for (const component in initialVisibility) {
+      const savedState = localStorage.getItem(getLocalStorageKey(component));
+      savedVisibility[component] =
+        savedState !== null
+          ? JSON.parse(savedState)
+          : initialVisibility[component];
+    }
+    return savedVisibility;
+  };
+
+  // Initialize visibility state with values from localStorage or defaults
+  const [visibility, setVisibility] = useState(loadVisibilityState);
+
+  // Toggle visibility and save the updated state to localStorage
   const toggleVisibility = (component) => {
-    setVisibility((prev) => ({
-      ...prev,
-      [component]: !prev[component],
-    }));
+    setVisibility((prev) => {
+      const newVisibility = !prev[component];
+      if (typeof window !== "undefined") {
+        localStorage.setItem(
+          getLocalStorageKey(component),
+          JSON.stringify(newVisibility),
+        );
+      }
+      return {
+        ...prev,
+        [component]: newVisibility,
+      };
+    });
   };
 
   return (
